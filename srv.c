2c0c60f84755448bab6516fd15f779fdec48dcc7
Eddy-C0de <eddy@volle.tech>
2024-11-24 19:45:13 +0100

    Socle du server web

--- srv.c
+++ srv.c
@@ -6,66 +6,76 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #define PORT 8000
+#define BUFFER_SIZE 512
+
+void handle_client(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_read = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Reçu du client : %s\n", buffer);
+    }
+
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello, world!";
+    send(client_socket, response, strlen(response), 0);
+    close(client_socket);
+}
 
 int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t taille_addr = sizeof(client_addr);
+	pthread_t thread_id;
 
-	int sct, req; 
-	struct sockaddr_in srv, client;
-	socklen_t taille_addr;
-	char buffer[512];
-	char *data;
+	// Création du socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket");
+        return -1;
+    }
 
-	//  Reservation de la memoire pour srv et client 
-	memset(&srv, 0, sizeof(srv));
-	memset(&client, 0, sizeof(client));
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	// Création du socket :)
-	sct = socket(AF_INET, SOCK_STREAM, 0);
-	if (sct < 0) {
-		printf("erreur de socket()\n");
-		return -1;
-	}
-	srv.sin_addr.s_addr = inet_addr("127.0.0.1");
-	srv.sin_port = htons(PORT); 
-	srv.sin_family = AF_INET; 
+	// J'asssigne une addresse au socket
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(server_socket);
+        return -1;
+    }
 
-	// J'assigne une local addresse à la socket
-	if (bind(sct, (struct sockaddr *)&srv, sizeof(srv))) {
-		printf("erreur lors du bind()\n");
-		return -1; 	
-	}
+    if (listen(server_socket, 5) < 0) {
+        perror("listen");
+        close(server_socket);
+        return -1;
+    }
 
-	// On se met en écoute avant d'accepter les connexions !!
-	if (listen(sct, 5)) {
-		printf("erreur lors du listen()\n");
-		close(sct);
-		return -1;
-	}
-	printf("En écoute sur 127.0.0.1:%d\n", PORT);
+    printf("En écoute sur 127.0.0.1:%d\n", PORT);
+
+	// Connexions multiples 
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &taille_addr);
+        if (client_socket < 0) {
+            perror("accept");
+            continue;
+        }
 	
-	// Connexions multiples
-	while (1) {
-		req = accept(sct, (struct sockaddr *)&srv, &taille_addr);
-		if (req < 0) {
-			printf("erreur lors de accept()\n"); 
-			close(sct);
-			return -1; 
-		}
-	}
+		printf("Client connecté depuis %s:%d\n",
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        handle_client(client_socket);
+    }
 
-	printf("Client connecté\n"); 
-	
-	// Envoie de données au client 
-	read(req, buffer, 512); 
-	data = "httpd v1.0\n";
-	write(req, data, strlen(data));
-
-	close(req);
-	close(sct);
-
-	return 0;
+    close(server_socket);
+    return 0;
 }
+
